151f25e95f1581fffed785d241f17489deb85be3
Ramelian <123Ramin321@gmail.com>
2024-04-23 16:08:49 +0200

    updated backend uploads

--- back/index.js
+++ back/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/uploads", express.static("uploads"));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 app.post("/product", BikeController.create);
 app.post("/contacts", OrderController.createContactMesssage);
